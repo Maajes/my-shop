0ec31ca33c28830a68525af05b816e8c2383f64e
Maajes <3371476457@qq.com>
2026-03-03 12:12:30 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(__dirname));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'Server is running!' });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
